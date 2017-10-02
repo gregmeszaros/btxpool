d5bbf67d57ff2d5bfb824b4aa41c18aa2a34b7f6
gregmeszaros <meszaros275@gmail.com>
2017-10-02 10:37:18 +0100

    Add composer and twig

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-// What page/algo we are on
-$algo = 'bitcore';
+// Add autoload
+require __DIR__ . '/vendor/autoload.php';
 
 // Connect mysql
-$conn = include('config.php');
+$conn = include_once('config.php');
 
 /**
  * Helper class
@@ -12,13 +12,28 @@ $conn = include('config.php');
  */
 include_once('minerHelper.php');
 
+// What page/algo we are on
+$algo = 'bitcore';
+
 // Gets data for specific miner address
 $miner_address = $_GET['address'] ?? "";
-$workers = minerHelper::getWorkers($conn, $miner_address);
-foreach ($workers as $worker) {
-  print 'Version: ' . $worker['version'];
 
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader, [
+  'cache' => __DIR__ . '/twig_cache',
+  // 'auto_reload' => true // Should be turned off on production
+]);
+
+// Get workers for miner address
+$workers = minerHelper::getWorkers($conn, $miner_address);
+
+foreach ($workers as $key => $worker) {
   $hashrate = minerHelper::getHashrate($conn, $algo, $worker['version'], $worker['name']);
-  print ' - hashrate: ' . minerHelper::Itoa2($hashrate['hashrate']) . 'h/s';
-  print '<br />';
+  $workers[$key]['hashrate'] = minerHelper::Itoa2($hashrate['hashrate']) . 'h/s';
 }
+
+// Return TWIG template
+print $twig->render('index.html.twig', [
+  'name' => 'Greg',
+  'workers' => $workers
+]);
