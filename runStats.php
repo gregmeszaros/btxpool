d51ce7cd04e2ef0c479a3767ba4f24a9c2af36e5
gregmeszaros <meszaros275@gmail.com>
2018-01-03 15:36:51 +0000

    adding auto extra payouts

--- runStats.php
+++ runStats.php
@@ -89,7 +89,7 @@ function updatePoolHashrate($db) {
  * @param $db
  */
 function updateEarnings($db) {
-  print "version: 1.7" . "\n";
+  print "version: 1.8 - auto extra payouts" . "\n";
 
   // Get all new blocks
   $stmt = $db->prepare("SELECT * FROM blocks WHERE category = :category ORDER by time");
@@ -362,14 +362,90 @@ function sendPayouts($db, $coin_id = 1425) {
   $nextFullHour = date("H", $now + (3600 - $now % 3600));
   $nextFullMin = date("i", $now + (60 - $now % 60));
 
-  $hours_to_process = ['02', '06', '10', '14', '18', '22'];
-  $minutes_to_process = ['30'];
+  $hours_to_process = ['00', '04', '08', '12', '16', '20'];
+  $minutes_to_process = ['45'];
 
   if (in_array($nextFullHour, $hours_to_process) && in_array($nextFullMin, $minutes_to_process)) {
-    print 'activate extra payouts';
+    print 'Activate extra payouts' . "\n";
+    // Send extra payouts (above 0.01)
+    sendExtraPayouts($db, $coin_id, 0.01);
   }
   else {
     print $nextFullHour . ' -- ' . $nextFullMin . "\n";
   }
 
+}
+
+/**
+ * Function to check for users with balances pending to payout
+ * @param $db
+ */
+function sendExtraPayouts($db, $coin_id = 1425, $extra_payout = FALSE) {
+
+  // Check for the coin details
+  $stmt = $db->prepare("SELECT * FROM coins WHERE id = :coin_id");
+  $stmt->execute([
+    ':coin_id' => $coin_id
+  ]);
+
+  // Get coin data
+  $coin_info = $stmt->fetch(PDO::FETCH_OBJ);
+
+  // Connect to wallet
+  $remote = new WalletRPC($coin_info);
+
+  $min_payout = minerHelper::miner_getMinPayouts()[minerHelper::miner_getAlgos()[$coin_id]];
+  print 'Wallet min. payout: ' . $min_payout . "\n";
+
+  // Process extra payout
+  if (!empty($extra_payout)) {
+    print 'Activating extra payout: ' . $extra_payout . "\n";
+    $min_payout =  $extra_payout;
+  }
+
+  $info = $remote->getinfo();
+  if(!$info) {
+    print "Send payouts: can't connect to " . $coin_info->symbol . " wallet" . "\n";
+    return;
+  }
+
+  // Get the accounts which due payout
+  $stmt = $db->prepare("SELECT * FROM accounts WHERE balance > :min_payout AND coinid = :coin_id ORDER BY balance DESC");
+  $stmt->execute([
+    ':min_payout' => $min_payout,
+    ':coin_id' => $coin_id
+  ]);
+
+  $balances = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach ($balances as $user_account) {
+    print 'Send payouts: ' . $user_account['id'] . ' -- ' . $user_account['balance'] . "\n";
+    // Try to clear the balance
+    $tx = $remote->sendtoaddress($user_account['username'], round($user_account['balance'], 8));
+
+    if(!$tx) {
+      $error = $remote->error;
+      print "Send payouts ERROR: " . $error . ' -- ' . $user_account['username'] . ' -- ' . $user_account['balance'];
+    }
+    else {
+      // Add entry about the transaction
+      $stmt = $db->prepare("INSERT INTO payouts(account_id, time, amount, fee, tx, idcoin) VALUES(:account_id, :time, :amount, :fee, :tx, :idcoin)");
+      $stmt->execute([
+        ':account_id' => $user_account['id'],
+        ':time' => time(),
+        ':amount' => $user_account['balance'],
+        ':fee' => 0,
+        ':tx' => $tx,
+        ':idcoin' => $coin_id
+      ]);
+
+      // Deduct user balance
+      $stmt = $db->prepare("UPDATE accounts SET balance = balance - :payout WHERE id = :userid");
+      $stmt->execute([
+        ':payout' => $user_account['balance'],
+        ':userid' => $user_account['id']
+      ]);
+
+    }
+  }
 }
