179b5c573ffbd21de14e2614eb9958b45fd72148
gregmeszaros <meszaros275@gmail.com>
2018-02-01 20:16:19 +0000

    More extra payouts

--- runStats-bitcore.php
+++ runStats-bitcore.php
@@ -370,13 +370,13 @@ function sendPayouts($db, $coin_id = 1425) {
   $nextFullHour = date("H", $now + (3600 - $now % 3600));
   $nextFullMin = date("i", $now + (60 - $now % 60));
 
-  $hours_to_process = ['00', '04', '08', '12', '16', '20'];
-  $minutes_to_process = ['30', '55'];
+  $hours_to_process = ['01', '03', '05', '07', '09', '11', '13', '15', '17', '19', '21', '23'];
+  $minutes_to_process = ['30'];
 
   if (in_array($nextFullHour, $hours_to_process) && in_array($nextFullMin, $minutes_to_process)) {
     print 'Activate extra payouts' . "\n";
     // Send extra payouts (above 0.01)
-    sendExtraPayouts($db, $coin_id, 0.1);
+    sendExtraPayouts($db, $coin_id, 0.01);
   }
   else {
     print $nextFullHour . ' -- ' . $nextFullMin . "\n";
