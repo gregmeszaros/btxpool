2693b2c111f98fee05005d82f79a269917ed6409
gregmeszaros <meszaros275@gmail.com>
2018-04-18 17:48:51 +0100

    update timing

--- runStats-gobyte.php
+++ runStats-gobyte.php
@@ -372,7 +372,7 @@ function sendPayouts($db, $coin_id = 1425) {
   $nextFullHour = date("H", $now + (3600 - $now % 3600));
   $nextFullMin = date("i", $now + (60 - $now % 60));
 
-  $hours_to_process = ['00', '04', '08', '12', '16', '20'];
+  $hours_to_process = ['01', '05', '09', '13', '17', '21'];
   $minutes_to_process = ['55'];
 
   if (in_array($nextFullHour, $hours_to_process) && in_array($nextFullMin, $minutes_to_process)) {
